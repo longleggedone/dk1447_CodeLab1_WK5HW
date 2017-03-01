1c449c9c2bc63727266b9a1ce6d5969ec04c1e1e
Daniel Kimsey <longlegsclassic@gmail.com>
2017-03-01 08:37:50 -0500

    Updated Script

--- Week 5 Assignment/Assets/Scripts/LevelLoaderScript.cs
+++ Week 5 Assignment/Assets/Scripts/LevelLoaderScript.cs
@@ -33,17 +33,56 @@ public class LevelLoaderScript : MonoBehaviour {
 
 			for(int xPos = 0; xPos < line.Length; xPos++){
 				if(line[xPos] == 'x'){
-					GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+					//GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
 
-					cube.transform.parent = levelHolder.transform;
+					GameObject obstacleHorizontal = Instantiate(Resources.Load("Prefabs/Obstacle1") as GameObject);
 
-					cube.transform.position = new Vector3(xPos + offsetX, yPos + offsetY, 0);
+					obstacleHorizontal.transform.parent = levelHolder.transform;
+
+					obstacleHorizontal.transform.position = new Vector3(xPos + offsetX, yPos + offsetY, 0);
+				}
+
+				if(line[xPos] == 'y'){
+					
+					GameObject obstacleVertical = Instantiate(Resources.Load("Prefabs/Obstacle2") as GameObject);
+
+					obstacleVertical.transform.parent = levelHolder.transform;
+
+					obstacleVertical.transform.position = new Vector3(xPos + offsetX, yPos + offsetY, 0);
+				}
+
+				if(line[xPos] == '+'){
+					
+					GameObject obstacleCross = Instantiate(Resources.Load("Prefabs/Obstacle3") as GameObject);
+
+					obstacleCross.transform.parent = levelHolder.transform;
+
+					obstacleCross.transform.position = new Vector3(xPos + offsetX, yPos + offsetY, 0);
+				}
+
+				if(line[xPos] == 'r'){
+					
+					GameObject obstacleRotating = Instantiate(Resources.Load("Prefabs/ObstacleRotating") as GameObject);
+
+					obstacleRotating.transform.parent = levelHolder.transform;
+
+					obstacleRotating.transform.position = new Vector3(xPos + offsetX, yPos + offsetY, 0);
+				}
+
+				if(line[xPos] == 'o'){
+					
+					GameObject points = Instantiate(Resources.Load("Prefabs/Points") as GameObject);
+
+					points.transform.parent = levelHolder.transform;
+
+					points.transform.position = new Vector3(xPos + offsetX, yPos + offsetY, 0);
 				}
 
 				if(line[xPos] == 'P'){
 					player.transform.position = new Vector3(
 						xPos + offsetX, yPos + offsetY, 0);
 				}
+
 			}
 			yPos--;
 		}
@@ -54,7 +93,7 @@ public class LevelLoaderScript : MonoBehaviour {
 	void Update () {
 		if(Input.GetKeyDown(KeyCode.Return)){
 			levelNum++;
-			SceneManager.LoadScene("Test");
+			SceneManager.LoadScene("LevelBuilder");
 		}
 	}
 }
